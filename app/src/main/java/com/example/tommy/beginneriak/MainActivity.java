4b968fc177cd18e90798e377c2ad15b9133e92dd
tommyputranto1 <tommyputranto1@gmail.com>
2016-04-16 00:50:41 +0700

    buat variabel

--- app/src/main/java/com/example/tommy/beginneriak/MainActivity.java
+++ app/src/main/java/com/example/tommy/beginneriak/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.tommy.beginneriak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button benarButton;
+    private Button salahButton;
+    private Button mTextPertanyaan;
+
+    private Question[] mQuestionsBank = new Question[]{
+            new Question(R.string.pertannyaan_jakarta, true),
+            new Question(R.string.pertannyaan_surabaya, true),
+            new Question(R.string.pertannyaan_salah, false),
+            new Question(R.string.pertannyaan_salah1, false),
+    };
+    private int mIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
