fc4a8fc02a7c39c1d90ccc0df85a254ecc259146
tommyputranto1 <tommyputranto1@gmail.com>
2016-04-16 00:06:42 +0700

    buat model dari question

--- app/src/main/java/com/example/tommy/beginneriak/Question.java
+++ app/src/main/java/com/example/tommy/beginneriak/Question.java
@@ -4,5 +4,27 @@ package com.example.tommy.beginneriak;
  * Created by tommy on 4/15/16.
  */
 public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
 
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
 }
